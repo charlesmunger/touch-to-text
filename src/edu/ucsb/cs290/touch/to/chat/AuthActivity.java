e327d4686b654ac3d831bdd2056bde85de66b540
Charles Munger <charleslmunger@gmail.com>
2012-10-17 18:19:43 -0700

    Removed useless menu from AuthActivity

--- src/edu/ucsb/cs290/touch/to/chat/AuthActivity.java
+++ src/edu/ucsb/cs290/touch/to/chat/AuthActivity.java
@@ -3,7 +3,6 @@ package edu.ucsb.cs290.touch.to.chat;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,12 +12,6 @@ public class AuthActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_auth, menu);
-        return true;
     }
     
     public void submitPassword(View view) {
