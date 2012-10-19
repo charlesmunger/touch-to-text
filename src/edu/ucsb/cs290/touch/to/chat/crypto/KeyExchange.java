97b2920d7dda61945e15c6b8b749e99cd45e394e
Charles Munger <charleslmunger@gmail.com>
2012-10-19 00:07:24 -0700

    Moved more key generation into the constructor, and thus out of the UI thread.

--- src/edu/ucsb/cs290/touch/to/chat/crypto/KeyExchange.java
+++ src/edu/ucsb/cs290/touch/to/chat/crypto/KeyExchange.java
@@ -8,6 +8,8 @@ import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.logging.Level;
@@ -17,32 +19,43 @@ import javax.crypto.KeyAgreement;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.DHParameterSpec;
 
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+
 public class KeyExchange implements Serializable {
 
+	static {
+		Security.addProvider(new BouncyCastleProvider());
+	}
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private KeyPair keypair;
+	private KeyAgreement ka;
+	private KeyFactory keyFact;
 	public KeyExchange() {
 		this(256);
 	}
-	
+
 	public KeyExchange(int keyBits) {
 		try {
-		AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
-        paramGen.init(keyBits);
+			AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator
+					.getInstance("DH");
+			paramGen.init(keyBits);
 
-        // Generate the parameters
-        AlgorithmParameters params = paramGen.generateParameters();
-        DHParameterSpec dhSpec
-            = (DHParameterSpec)params.getParameterSpec(DHParameterSpec.class);
+			// Generate the parameters
+			AlgorithmParameters params = paramGen.generateParameters();
+			DHParameterSpec dhSpec = (DHParameterSpec) params
+					.getParameterSpec(DHParameterSpec.class);
 
-		    // Use the values to generate a key pair
-		    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DH");
-		    keyGen.initialize(dhSpec);
-		    keypair = keyGen.generateKeyPair();
-		} catch(Exception e) {
+			// Use the values to generate a key pair
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DH");
+			keyGen.initialize(dhSpec);
+			keypair = keyGen.generateKeyPair();
+			ka = KeyAgreement.getInstance("DH", "SC");
+			keyFact = KeyFactory.getInstance("DH", "SC");
+		    ka.init(keypair.getPrivate());
+		} catch (Exception e) {
 			Logger.getLogger("touch-to-text").log(Level.SEVERE, "Exception in key exchange", e);
 		}
 	}
@@ -51,17 +64,13 @@ public class KeyExchange implements Serializable {
 	    return keypair.getPublic().getEncoded();
 	}
 	
-	public SecretKey setOtherPublicKey(byte[] b,String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException {
+	public SecretKey setOtherPublicKey(byte[] b,String algorithm) throws NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, NoSuchProviderException {
 		if(keypair == null) {
 			throw new UnsupportedOperationException("Do not reuse key exchange objects.");
 		}
 	    // Convert the public key bytes into a PublicKey object
 	    X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(b);
-	    KeyFactory keyFact = KeyFactory.getInstance("DH");
-
 	    // Prepare to generate the secret key with the private key and public key of the other party
-	    KeyAgreement ka = KeyAgreement.getInstance("DH");
-	    ka.init(keypair.getPrivate());
 	    ka.doPhase(keyFact.generatePublic(x509KeySpec), true);
 	    // Generate the secret key
 	    keypair = null; //Ensure not reused
