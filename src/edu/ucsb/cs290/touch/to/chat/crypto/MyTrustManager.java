4d715c9bb1acc0652cf74dc513df297e1ca87abf
Charles Munger <charleslmunger@gmail.com>
2012-11-06 22:54:01 -0800

    Added code to securely POST to the server.

---
+++ src/edu/ucsb/cs290/touch/to/chat/crypto/MyTrustManager.java
@@ -0,0 +1,160 @@
+package edu.ucsb.cs290.touch.to.chat.crypto;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateExpiredException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
+import android.util.Log;
+
+public class MyTrustManager implements X509TrustManager {
+	private KeyStore keyStore;
+	private X509TrustManager defaultTrustManager;
+	private X509TrustManager appTrustManager;
+	private final String LOG = "MyTrustManager";
+	byte[] keyStored = null;
+	String pwd;
+	
+	public MyTrustManager() {
+		loadKeyStore();
+		
+		defaultTrustManager = getTrustManager(false);
+		appTrustManager = getTrustManager(true);
+	}
+	
+	private X509TrustManager getTrustManager(boolean withKeystore) {
+		try {
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+			if(withKeystore)
+				tmf.init(keyStore);
+			else
+				tmf.init((KeyStore) null);
+			for(TrustManager t : tmf.getTrustManagers())
+				if(t instanceof X509TrustManager)
+					return (X509TrustManager) t;
+		} catch (KeyStoreException e) {
+			Log.e(LOG, "key store exception: " + e.toString());
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG, "no such algo exception: " + e.toString());
+		}
+		return null;
+	}
+	
+	private void loadKeyStore() {
+		//TODO: this is where you load up your keystore and store the bytes into the keyStored field if neccessary.
+		try {
+			keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		} catch(KeyStoreException e) {
+			Log.e(LOG, "key store exception: " + e.toString());
+		}
+		
+		try {
+			keyStore.load(null, null);
+			if(keyStored != null)
+				keyStore.load(new ByteArrayInputStream(keyStored), pwd.toCharArray());
+			
+			
+		} catch(CertificateException e) {
+			Log.e(LOG, "certificate exception: " + e.toString());
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG, "no such algo exception: " + e.toString());
+		} catch (IOException e) {
+			Log.e(LOG, "IOException: " + e.toString());
+		}
+	}
+	
+	private void storeCertificate(X509Certificate[] chain) {
+		try {
+			for(X509Certificate cert : chain) {
+				keyStore.setCertificateEntry(cert.getSubjectDN().toString(), cert);
+			}
+		} catch(KeyStoreException e) {
+			Log.e(LOG, "keystore exception: " + e.toString());
+		}
+		
+		appTrustManager = getTrustManager(true);
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			keyStore.store(baos, pwd.toCharArray());
+			updateKeyStore(baos.toByteArray());
+			Log.d(LOG, "new key encountered!  length: " + baos.size());
+		} catch(KeyStoreException e) {
+			Log.e(LOG, "keystore exception: " + e.toString());	
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG, "no such algo exception: " + e.toString());
+		} catch (IOException e) {
+			Log.e(LOG, "IOException: " + e.toString());
+		} catch (CertificateException e) {
+			Log.e(LOG, "Certificate Exception: " + e.toString());
+		}
+	}
+	
+	private void updateKeyStore(byte[] newKey) {
+		// TODO: this is where YOU update your own keystore if you need to (ie, if it's in an SQLite database)
+	}
+	
+	private boolean isCertKnown(X509Certificate cert) {
+		try {
+			return keyStore.getCertificateAlias(cert) != null;
+		} catch(KeyStoreException e) {
+			return false;
+		}
+	}
+	
+	private boolean isExpiredException(Throwable e) {
+		do {
+			if(e instanceof CertificateExpiredException)
+				return true;
+			e = e.getCause();
+		} while(e != null);
+		return false;
+	}
+	
+	private void checkCertificateTrusted(X509Certificate[] chain, String authType, boolean isServer) throws CertificateException {
+		try {
+			if(isServer)
+				appTrustManager.checkServerTrusted(chain, authType);
+			else
+				appTrustManager.checkClientTrusted(chain, authType);
+		} catch(CertificateException e) {
+			if(isExpiredException(e))
+				return;
+			if(isCertKnown(chain[0]))
+				return;
+			
+			try {
+				if(isServer)
+					defaultTrustManager.checkServerTrusted(chain, authType);
+				else
+					defaultTrustManager.checkClientTrusted(chain, authType);
+			} catch(CertificateException ce) {
+				storeCertificate(chain);
+			}
+		}
+	}
+
+	@Override
+	public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		checkCertificateTrusted(chain, authType, false);
+	}
+
+	@Override
+	public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+		checkCertificateTrusted(chain, authType, true);
+	}
+
+	@Override
+	public X509Certificate[] getAcceptedIssuers() {
+		return defaultTrustManager.getAcceptedIssuers();
+	}
+	
+}
---
+++ src/edu/ucsb/cs290/touch/to/chat/https/TorProxy.java
@@ -0,0 +1,162 @@
+package edu.ucsb.cs290.touch.to.chat.https;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+
+import android.util.Log;
+import edu.ucsb.cs290.touch.to.chat.crypto.MyTrustManager;
+import edu.ucsb.cs290.touch.to.chat.remote.messages.TokenAuthMessage;
+import edu.ucsb.cs290.touch.to.chat.remote.register.RegisterUser;
+
+public class TorProxy {
+	private static final String serverUrl = "k4pf4spu5742f4px.onion";
+	private static final String LOG = "TorProxy";
+	private static final ExecutorService ex = Executors.newCachedThreadPool();
+	public static String sendMessage(TokenAuthMessage m) {
+		Map<String,Object>tempMap = new HashMap<String, Object>(1);
+		tempMap.put("mesage", m);
+		return executeHttpsPost(serverUrl, tempMap, "message");
+	}
+	
+	public static String registerUser(RegisterUser r) {
+		Map<String,Object>tempMap = new HashMap<String, Object>(1);
+		tempMap.put("register", r);
+		return executeHttpsPost(serverUrl, tempMap, "message");
+	}
+	
+	private static String executeHttpsPost(final String host, final Map<String,Object> postData, final String contentType) {
+		Future<String> future = ex.submit(new Callable<String>() {
+			String result = "FAIL";
+			String HYPHENS = "--";
+			String LINE_END = "\r\n";
+			String BOUNDARY = "***7hisIsMyBoUND4rY***";
+			String hostname;
+			
+			URL url;
+			HttpsURLConnection connection;
+			HostnameVerifier hnv;
+			DataOutputStream dos;
+			SSLContext ssl;
+			
+			MyTrustManager itm;
+			
+			private void buildQuery() {
+				Iterator<Entry<String, Object>> it = postData.entrySet().iterator();
+				
+				connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+				StringBuffer sb = new StringBuffer();
+				try {
+					dos = new DataOutputStream(connection.getOutputStream());
+					sb = new StringBuffer();
+					while(it.hasNext()) {
+						sb = new StringBuffer();
+						Entry<String, Object> e = it.next();
+						
+						sb.append(HYPHENS + BOUNDARY + LINE_END);
+						
+						sb.append("Content-Disposition: form-data; name=\"" + e.getKey() + "\"" + LINE_END);
+						sb.append("Content-Type: " + contentType + "; charset=UTF-8" + LINE_END );
+						sb.append("Cache-Control: no-cache" + LINE_END + LINE_END);
+						sb.append(String.valueOf(e.getValue()) + LINE_END);
+						dos.writeBytes(sb.toString());
+					}
+					
+					dos.writeBytes(HYPHENS + BOUNDARY + HYPHENS + LINE_END);
+					
+					dos.flush();
+					dos.close();
+					
+				} catch (IOException e) {
+					Log.e(LOG, e.toString());
+					e.printStackTrace();
+				}
+			}
+			
+			@Override
+			public String call() throws Exception {
+				hostname = host.split("/")[0];
+				url = new URL("https://" + host);
+								
+				hnv = new HostnameVerifier() {
+					@Override
+					public boolean verify(String hn, SSLSession session) {
+						if(hn.equals(hostname))
+							return true;
+						else
+							return false;
+					}
+				};
+				
+				itm = new MyTrustManager();
+								
+				ssl = SSLContext.getInstance("TLS");
+				ssl.init(null, new TrustManager[] {itm}, new SecureRandom());
+				
+				HttpsURLConnection.setDefaultSSLSocketFactory(ssl.getSocketFactory());
+				HttpsURLConnection.setDefaultHostnameVerifier(hnv);
+				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8118));
+				
+				connection = (HttpsURLConnection) url.openConnection(proxy);
+				
+				connection.setRequestMethod("POST");
+				connection.setRequestProperty("Connection", "Keep-Alive");
+				connection.setUseCaches(false);
+				connection.setDoInput(true);
+				connection.setDoOutput(true);
+				
+				buildQuery();
+				
+				try {
+					InputStream is = connection.getInputStream();
+					BufferedReader br = new BufferedReader(new InputStreamReader(is));
+					String line;
+					StringBuffer sb = new StringBuffer();
+					while((line = br.readLine()) != null)
+						sb.append(line);
+					br.close();
+					connection.disconnect();
+					result = sb.toString();
+				} catch(NullPointerException e) {
+					Log.e(LOG, e.toString());
+					e.printStackTrace();
+				}
+				return result;
+			}
+			
+		});
+		
+		try {
+			return future.get();
+		} catch (InterruptedException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+			return null;
+		} catch (ExecutionException e) {
+			Log.e(LOG, e.toString());
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
