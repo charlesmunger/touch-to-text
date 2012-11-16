176741c1683370798e0c94b20045ac842358ef41
dannyiland <dannyiland@gmail.com>
2012-11-15 21:36:21 -0800

    Implemented SSL Certificate pinning

--- src/edu/ucsb/cs290/touch/to/chat/https/TorProxy.java
+++ src/edu/ucsb/cs290/touch/to/chat/https/TorProxy.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -18,13 +20,19 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.util.Log;
 import edu.ucsb.cs290.touch.to.chat.remote.messages.TokenAuthMessage;
 import edu.ucsb.cs290.touch.to.chat.remote.register.RegisterUser;
@@ -32,19 +40,52 @@ import edu.ucsb.cs290.touch.to.chat.remote.register.RegisterUser;
 public class TorProxy {
 	private static final String serverUrl = "k4pf4spu5742f4px.onion";
 	private static final String LOG = "TorProxy";
+	private static final String KEYSTORE = "touch-to-text.store";
 	private static final ExecutorService ex = Executors.newCachedThreadPool();
 	public static String sendMessage(TokenAuthMessage m) {
 		Map<String,Object>tempMap = new HashMap<String, Object>(1);
-		tempMap.put("mesage", m);
+		tempMap.put("message", m);
 		return executeHttpsPost(serverUrl, tempMap, "message");
 	}
-	
+
 	public static String registerUser(RegisterUser r) {
 		Map<String,Object>tempMap = new HashMap<String, Object>(1);
 		tempMap.put("register", r);
 		return executeHttpsPost(serverUrl, tempMap, "message");
 	}
-	
+
+	/**
+	 * Moxie's recommended way to implement cert pinning
+	 * Returns the SSLSocketFactory that can be used
+	 * to initialize a HttpsURLConnection.
+	 * 
+	 * @param context The application context, used to get
+	 * touch-to-text.store from the assets directory.
+	 * 
+	 * @return The SSLSocketFactory, or null if something bad happened.
+	 */
+	private SSLSocketFactory getPinnedSSLSocketFactory(Context context) {
+		AssetManager assetManager = context.getAssets();
+		try {
+			InputStream keyStoreInputStream = assetManager.open(KEYSTORE);
+			KeyStore trustStore = KeyStore.getInstance("BKS");
+			trustStore.load(keyStoreInputStream, "nopass".toCharArray());
+			TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+			tmf.init(trustStore);
+			SSLContext sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, tmf.getTrustManagers(), null);
+			return sslContext.getSocketFactory();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			Logger.getLogger("touch-to-text").log(Level.SEVERE, 
+					"Error accessing pinned certificate.", e);
+		} catch (GeneralSecurityException e) {
+			Logger.getLogger("touch-to-text").log(Level.SEVERE, 
+					"The server certificate could not be validated.", e);
+		}
+		return null;
+	}
+
 	private static String executeHttpsPost(final String host, final Map<String,Object> postData, final String contentType) {
 		Future<String> future = ex.submit(new Callable<String>() {
 			String result = "FAIL";
@@ -52,18 +93,18 @@ public class TorProxy {
 			String LINE_END = "\r\n";
 			String BOUNDARY = "***7hisIsMyBoUND4rY***";
 			String hostname;
-			
+
 			URL url;
 			HttpsURLConnection connection;
 			HostnameVerifier hnv;
 			DataOutputStream dos;
 			SSLContext ssl;
-			
+
 			MyTrustManager itm;
-			
+
 			private void buildQuery() {
 				Iterator<Entry<String, Object>> it = postData.entrySet().iterator();
-				
+
 				connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
 				StringBuffer sb = new StringBuffer();
 				try {
@@ -72,32 +113,32 @@ public class TorProxy {
 					while(it.hasNext()) {
 						sb = new StringBuffer();
 						Entry<String, Object> e = it.next();
-						
+
 						sb.append(HYPHENS + BOUNDARY + LINE_END);
-						
+
 						sb.append("Content-Disposition: form-data; name=\"" + e.getKey() + "\"" + LINE_END);
 						sb.append("Content-Type: " + contentType + "; charset=UTF-8" + LINE_END );
 						sb.append("Cache-Control: no-cache" + LINE_END + LINE_END);
 						sb.append(String.valueOf(e.getValue()) + LINE_END);
 						dos.writeBytes(sb.toString());
 					}
-					
+
 					dos.writeBytes(HYPHENS + BOUNDARY + HYPHENS + LINE_END);
-					
+
 					dos.flush();
 					dos.close();
-					
+
 				} catch (IOException e) {
 					Log.e(LOG, e.toString());
 					e.printStackTrace();
 				}
 			}
-			
+
 			@Override
 			public String call() throws Exception {
 				hostname = host.split("/")[0];
 				url = new URL("https://" + host);
-								
+
 				hnv = new HostnameVerifier() {
 					@Override
 					public boolean verify(String hn, SSLSession session) {
@@ -107,26 +148,26 @@ public class TorProxy {
 							return false;
 					}
 				};
-				
+
 				itm = new MyTrustManager();
-								
+
 				ssl = SSLContext.getInstance("TLS");
 				ssl.init(null, new TrustManager[] {itm}, new SecureRandom());
-				
+
 				HttpsURLConnection.setDefaultSSLSocketFactory(ssl.getSocketFactory());
 				HttpsURLConnection.setDefaultHostnameVerifier(hnv);
 				Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8118));
-				
+
 				connection = (HttpsURLConnection) url.openConnection(proxy);
-				
+
 				connection.setRequestMethod("POST");
 				connection.setRequestProperty("Connection", "Keep-Alive");
 				connection.setUseCaches(false);
 				connection.setDoInput(true);
 				connection.setDoOutput(true);
-				
+
 				buildQuery();
-				
+
 				try {
 					InputStream is = connection.getInputStream();
 					BufferedReader br = new BufferedReader(new InputStreamReader(is));
@@ -143,9 +184,9 @@ public class TorProxy {
 				}
 				return result;
 			}
-			
+
 		});
-		
+
 		try {
 			return future.get();
 		} catch (InterruptedException e) {
