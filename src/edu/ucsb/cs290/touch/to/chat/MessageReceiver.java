f0d9e3ac8ec1c2f5ede8666873c580ba6ad55fbf
Charles Munger <charleslmunger@gmail.com>
2012-12-09 00:17:44 -0800

    Added receiver.

---
+++ src/edu/ucsb/cs290/touch/to/chat/MessageReceiver.java
@@ -0,0 +1,17 @@
+package edu.ucsb.cs290.touch.to.chat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+
+public class MessageReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+		PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MessageReceived");
+		context.startService(intent);
+	}
+
+}
